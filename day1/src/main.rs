713e529cb5b7ce35d4d1356b39765c89dd1a30d2
KyleStiers <kylestiers@gmail.com>
2025-12-04 21:46:50 -0600

    day1 completed

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,19 +3,46 @@ use std::io::{self, BufRead, BufReader};
 use std::path::Path;
 use regex::Regex;
 
-fn find_new_pos(current_pos:i32) -> i32 {
+fn find_new_pos(current_pos:i32, movement:(char,i32)) -> i32 {
+    let pos = current_pos;
+    let (direction,initial_units) = movement;
 
+    let mut units = 0;
+
+    if initial_units.abs() > 99 {
+        units = initial_units % 100;    
+    } else {
+        units = initial_units;
+    }
+
+    if direction == 'L' {
+        let result = pos - units;
+        if result < 0 {
+            return 100 - result.abs();
+        }
+        else {
+            return result;
+        }
+    } else {
+        let result = pos + units;
+        if result > 99 {
+            return result - 100;
+        }
+        else {
+            return result;
+        }
+    }
 }
 
 fn main() -> io::Result<()> {
     // Specify the path to your text file
     let file_path = Path::new("input.txt");
-
+    let mut counter = 0;
     // Open the file
     let file = File::open(file_path)?;
     let reader = BufReader::new(file);
 
-    let pos : i32 = 50;
+    let mut pos : i32 = 50;
 
     // Loop over each line in the file
     for line_result in reader.lines() {
@@ -26,10 +53,18 @@ fn main() -> io::Result<()> {
         if let Some(captures) = re.captures(&line) {
             let direction = captures.get(1).map(|m| m.as_str()).unwrap_or("UP");
             let ticks = captures.get(2).map(|m| m.as_str()).unwrap_or("-1");
-            println!("Direction: {}, Ticks: {}", direction, ticks);
+            //println!("Direction: {}, Ticks: {}", direction, ticks);
+            pos = find_new_pos(pos, (direction.chars().next().unwrap(),ticks.parse().expect("Failed to parse")));
+            //println!("- The dial is rotated {}{} to point at {}", direction, ticks, pos);
+            if pos == 0 {
+                counter += 1;
+                println!("New value: {}", counter);
+            } else {}
         } else {
-            println!("We ain't found shit");
+            println!("We done or ain't found shit");
         }
+        //pos = find_new_pos(direction,ticks.parse().expect("Failed to parse"));
+        //println!("New value: {}", pos);
     }
 
     Ok(())
