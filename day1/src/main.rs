872f1552109c90a0c9803d9cc828d287ce2897ea
KyleStiers <kylestiers@gmail.com>
2025-12-06 08:31:33 -0600

    unfinished d1p2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,14 +3,20 @@ use std::io::{self, BufRead, BufReader};
 use std::path::Path;
 use regex::Regex;
 
-fn find_new_pos(current_pos:i32, movement:(char,i32)) -> i32 {
+fn increment(count:&mut i32) {
+    *count += 1;
+}
+
+fn find_new_pos(current_pos:i32, movement:(char,i32), p2_counter:&mut i32) -> i32 {
     let pos = current_pos;
     let (direction,initial_units) = movement;
 
     let mut units = 0;
 
     if initial_units.abs() > 99 {
-        units = initial_units % 100;    
+        units = initial_units % 100;
+        //let full_turns = initial_units / 100;
+        *p2_counter += initial_units / 100;
     } else {
         units = initial_units;
     }
@@ -18,6 +24,7 @@ fn find_new_pos(current_pos:i32, movement:(char,i32)) -> i32 {
     if direction == 'L' {
         let result = pos - units;
         if result < 0 {
+            *p2_counter += 1;
             return 100 - result.abs();
         }
         else {
@@ -25,7 +32,8 @@ fn find_new_pos(current_pos:i32, movement:(char,i32)) -> i32 {
         }
     } else {
         let result = pos + units;
-        if result > 99 {
+        if result > 100 {
+            *p2_counter += 1;
             return result - 100;
         }
         else {
@@ -37,7 +45,9 @@ fn find_new_pos(current_pos:i32, movement:(char,i32)) -> i32 {
 fn main() -> io::Result<()> {
     // Specify the path to your text file
     let file_path = Path::new("input.txt");
-    let mut counter = 0;
+    let mut protocol1_counter : i32 = 0;
+    let mut protocol2_counter : i32 = 0;
+
     // Open the file
     let file = File::open(file_path)?;
     let reader = BufReader::new(file);
@@ -54,18 +64,20 @@ fn main() -> io::Result<()> {
             let direction = captures.get(1).map(|m| m.as_str()).unwrap_or("UP");
             let ticks = captures.get(2).map(|m| m.as_str()).unwrap_or("-1");
             //println!("Direction: {}, Ticks: {}", direction, ticks);
-            pos = find_new_pos(pos, (direction.chars().next().unwrap(),ticks.parse().expect("Failed to parse")));
+            pos = find_new_pos(pos, (direction.chars().next().unwrap(),ticks.parse().expect("Failed to parse")), &mut protocol2_counter);
             //println!("- The dial is rotated {}{} to point at {}", direction, ticks, pos);
             if pos == 0 {
-                counter += 1;
-                println!("New value: {}", counter);
+                //protocol1_counter += 1;
+                increment(&mut protocol1_counter);
+                increment(&mut protocol2_counter);
+                //println!("New value: {}i", counter);
             } else {}
         } else {
             println!("We done or ain't found shit");
         }
         //pos = find_new_pos(direction,ticks.parse().expect("Failed to parse"));
-        //println!("New value: {}", pos);
     }
 
+    println!("Protocol 1 value: {}\tProtocol 2 value: {}", protocol1_counter, protocol2_counter);
     Ok(())
 }
