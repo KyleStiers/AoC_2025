1b5488a999c340ac8f1f163178efef99136128c1
Kyle Stiers (GNOCB) <kyle.stiers@bayer.com>
2025-12-08 16:14:34 -0600

    working day 3 p1

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -2,9 +2,24 @@ use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 use std::path::Path;
 
+fn highest_joltage(substring: &str, vacancies:i32) -> (String, usize) {
+    let mut idx :usize = 0;
+    let mut max :i32 = 0;
+    let numbers = substring.chars().collect::<Vec<char>>();
+    println!("Now checking substring: {}", substring);
+    for i in 0..numbers.len() {
+        let curr_num = numbers[i].to_digit(10).unwrap() as i32;
+        if curr_num > max && i <= (substring.len() - vacancies as usize) {
+            max = curr_num;
+            idx = i as usize;
+        }
+    }
+    return (max.to_string(),idx+1);
+}
+
 fn main() -> io::Result<()> {
     // Specify the path to your text file
-    let file_path = Path::new("test.txt");
+    let file_path = Path::new("input.txt");
 
     // Open the file
     let file = File::open(file_path)?;
@@ -15,11 +30,11 @@ fn main() -> io::Result<()> {
 
     // Loop over each line in the file
     for line_result in reader.lines() {
+        let max_batteries_on: i32 = 2;
         let line = line_result?;
-        let max_on :i32 = 2;
-        let values_as_char = vec!['0','1','2','3','4','5','6','7','8','9'];
-        let mut top_joltage :i32 = 0;
-        let mut top_vals: Vec<i32> = Vec::new();
+        let mut batteries_left :i32 = 2;
+        let mut top_vals = vec![];
+        let mut last_index: usize = 0;
 
         if line.len() == 1 {
             total += line.parse::<i32>().unwrap() as i64; //if only one digit it must be the highest
@@ -27,15 +42,20 @@ fn main() -> io::Result<()> {
         }
 
         println!("{}",line);
-        let mut last_highest_index = 0;
-        for val in values_as_char.into_iter().rev() {
-            let idx = line.find(val);
-            match idx {
-                Some(i) => last_highest_index = idx.expect("REASON"),
-                None => println!("{} not in this bank",val),
+        for on in 0..max_batteries_on {
+            if batteries_left == 0 {
+                break
             }
+            let mut val_to_add:String = String::new();
+            (val_to_add,last_index) = highest_joltage(&line[last_index as usize..], batteries_left);
+            top_vals.push(val_to_add);
+            batteries_left -= 1;
         }
-    }
 
+        let concatenated_batteries = top_vals.join("").parse::<i64>().unwrap();
+        total += concatenated_batteries;
+        println!("Top batteries: {}",concatenated_batteries);
+    }
+    println!("Total joltage: {}",total);
     Ok(())
 }
