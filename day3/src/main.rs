8469273043b3c2d56cc591478b68334b977ce769
KyleStiers <kylestiers@gmail.com>
2025-12-07 21:48:50 -0600

    progress so far

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -18,16 +18,22 @@ fn main() -> io::Result<()> {
         let line = line_result?;
         let max_on :i32 = 2;
         let values_as_char = vec!['0','1','2','3','4','5','6','7','8','9'];
-        let mut top_joltage : i32 = 0;
+        let mut top_joltage :i32 = 0;
+        let mut top_vals: Vec<i32> = Vec::new();
 
         if line.len() == 1 {
             total += line.parse::<i32>().unwrap() as i64; //if only one digit it must be the highest
             continue;
         }
 
+        println!("{}",line);
+        let mut last_highest_index = 0;
         for val in values_as_char.into_iter().rev() {
-            let index = line.iter().position(|c| c == &val);
-            
+            let idx = line.find(val);
+            match idx {
+                Some(i) => last_highest_index = idx.expect("REASON"),
+                None => println!("{} not in this bank",val),
+            }
         }
     }
 
