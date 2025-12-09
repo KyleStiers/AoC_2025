b942c57634639dc4086d06e72a4c527d03e6f5ce
Kyle Stiers (GNOCB) <kyle.stiers@bayer.com>
2025-12-08 22:09:09 -0600

    pseudocode for adjacent test

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -2,6 +2,22 @@ use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 use std::path::Path;
 
+fn check_position {
+    // remap
+
+    // if row is lower bound
+        // sum symbols in [row][col -1][row][col+1],[row +1][col -1..+1]
+    // if col is lower bound
+        // sum symbols in [row - 1][col..col+1],[row][col+1],[row +1][col..col+1]
+    // if row is upper bound
+        // sum symbols in [row - 1][col -1..+1],[row][col -1][row][col+1]
+    // if col is upper bound
+        // sum symbols in [row - 1][col-1..col],[row][col-1],[row +1][col-1..col]
+    // inside the grid
+        // sum symbols in [row - 1][col -1..+1],[row][col -1][row][col+1],[row +1][col -1..+1]
+}
+
+
 fn main() -> io::Result<()> {
     // Specify the path to your text file
     let file_path = Path::new("test.txt");
