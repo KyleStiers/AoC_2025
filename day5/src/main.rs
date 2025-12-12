a85f959170d5895fbc912da06c455ee5f9112249
Kyle Stiers (GNOCB) <kyle.stiers@bayer.com>
2025-12-12 14:33:10 -0600

    day 5 part 2 done

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -4,18 +4,22 @@ use std::path::Path;
 use regex::Regex;
 use rust_lapper::{Interval, Lapper};
 
+type Iv = Interval<usize,usize>;
+
 fn main() -> io::Result<()> {
     // Specify the path to your text file
-    let file_path = Path::new("test.txt");
+    let file_path = Path::new("input.txt");
     
     // Open the file
     let file = File::open(file_path)?;
     let reader = BufReader::new(file);
 
     // Set up some variables to store things in
-    let mut ranges: Vec<String> =  Vec::<String>::new();
-    let mut avail_items: Vec<i32> = Vec::<i32>::new();
-    // let mut intervals: Vec<Interval<i32,i32>> = Vec::<Interval<i32,i32>>::new();
+    let mut int_ranges: Vec<(usize,usize)> = Vec::<(usize,usize)>::new();
+    let mut intervals: Vec<Iv> = Vec::<Iv>::new();
+    let mut avail_items: Vec<usize> = Vec::<usize>::new();
+    let mut total_available: usize = 0;
+
 
     // Loop over each line in the file
     for line_result in reader.lines() {
@@ -28,25 +32,39 @@ fn main() -> io::Result<()> {
         let avail_matches = re_avail.find_iter(&line).map(|m| m.as_str()).collect::<Vec<&str>>();
         if ranges_matched.len() > 0 {
             for range in ranges_matched {
-                ranges.push(range.to_string());
+                // ranges.push(range.to_string());
+                let parts: Vec<usize> = range.split('-').map(|s| s.parse::<usize>().unwrap()).collect();
+                assert!(parts.len() == 2);
+                let found_interval = Iv{start:parts[0], stop:parts[1]+1, val:0};
+                
+                intervals.push(found_interval);
+                int_ranges.push((parts[0], parts[1]));
             }
         }
         if avail_matches.len() > 0 {
             for item in avail_matches {
-                avail_items.push(item.parse::<i32>().unwrap());
+                avail_items.push(item.parse::<usize>().unwrap());
             }
         }
     }
-    println!("Found range: {:?}", ranges);
-    println!("Found available items: {:?}", avail_items);
 
-    // Playing with intervals
-    type Iv = Interval<usize,usize>;
-    let data = vec![Iv{start: 5, stop: 12, val: 0}, Iv{start: 7, stop: 20, val: 0}];
-    let mut lapper = Lapper::new(data);
-    lapper.merge_overlaps();
-    let result = lapper.find(11,11).collect::<Vec<_>>();
-    println!("Lapper search result: {:?}", result);
+    // Example on how to use lapper from the docs
+    // let data = vec![Iv{start: 5, stop: 12, val: 0}, Iv{start: 7, stop: 20, val: 0}];
+    let mut lapper = Lapper::new(intervals);
+    lapper.merge_overlaps(); //man this method is awesome
+
+    // Now check if the items are fresh or spoiled
+    for item in avail_items {
+        let result = lapper.find(item, item).collect::<Vec<_>>();
+        if result.len() > 0 {
+            // println!("Item {} found in ranges: {:?}", item, result);
+            total_available += 1;
+        }
+    }
+
+    println!("Total fresh items: {}", total_available);
+    let other_total: usize = lapper.iter().map(|iv| iv.stop - iv.start).sum();
+    println!("Total items in ranges: {}", other_total);
 
     Ok(())
 }
