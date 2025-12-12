fd18d98bb1fd1a41960f795c53f67b99f068ce08
Kyle Stiers (GNOCB) <kyle.stiers@bayer.com>
2025-12-11 21:34:56 -0600

    day 4 part 2 done

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -53,53 +53,64 @@ fn main() -> io::Result<()> {
     // Open the file
     let file = File::open(file_path)?;
     let reader = BufReader::new(file);
-    let lines: Vec<String> = reader.lines().collect::<Result<_, _>>()?;
+    let mut lines: Vec<String> = reader.lines().collect::<Result<_, _>>()?;
 
     let rows = lines.len();
     let cols = lines[0].len();
-    println!("Total lines: {}, Total rows: {}", rows, cols);
-    let mut total_accessible :i32 = 0;
-    let mut input_matrix: Vec<Vec<i32>> = vec![vec![0; cols]; rows];
-    // input_matrix.iter_mut().for_each(|x| if x=='@' { *x = 1} else { *x = 0});
-    // println!("Initialized matrix: {:?}", input_matrix);
-    // let mut rows :usize = 0;
+    // println!("Total lines: {}, Total rows: {}", rows, cols);
 
-    for (index, line) in lines.iter().enumerate() {
-        for i in 0..line.len() {
-            let curr_char = line.chars().nth(i).unwrap();
-            if curr_char == '@' {
-                input_matrix[index][i] = 1;
-                // let is_ready :bool= check_position(index, i, &input_matrix);
-                // println!("Is ready: {}", is_ready);
-            } else {
-                input_matrix[index][i] = 0;
+    let mut total_rolls_removed :i32 = 0;
+    let mut input_matrix: Vec<Vec<i32>> = vec![vec![0; cols]; rows];
+    let mut copy_matrix: Vec<Vec<i32>> = vec![vec![0; cols]; rows];
+
+
+
+
+    loop { // TODO make this a loop that updates the matrix and then tries another round of refinement
+
+        // Load up the initial matrix
+        for (index, line) in lines.iter().enumerate() {
+            for i in 0..line.len() {
+                let curr_char = line.chars().nth(i).unwrap();
+                if curr_char == '@' {
+                    input_matrix[index][i] = 1;
+                } else {
+                    input_matrix[index][i] = 0;
+                }
             }
         }
-    }
-    println!("Rows: {:?}", input_matrix);
-    
-    for (row_index, row) in input_matrix.iter().enumerate() {
-        let mut available: i32;
-        let mut stringified_row = String::new();
-        for (col_index,col_val) in row.iter().enumerate() {
-            if *col_val == 1 {
-                available = check_adjacents(row_index, col_index, &input_matrix);
-                if available < 4 {
-                    total_accessible += 1;
-                    stringified_row.push('x')
+
+        // println!("Rows: {:?}", input_matrix);
+        let mut tmp_vec = Vec::<String>::new();
+        for (row_index, row) in input_matrix.iter().enumerate() {
+            let mut adjacent_rolls: i32;
+            let mut stringified_row = String::new();
+            for (col_index,col_val) in row.iter().enumerate() {
+                if *col_val == 1 {
+                    adjacent_rolls = check_adjacents(row_index, col_index, &input_matrix);
+                    if adjacent_rolls < 4 {
+                        total_rolls_removed += 1;
+                        stringified_row.push('x')
+                    } else{
+                        stringified_row.push('@')
+                    }
+                    // println!("Position ({},{}) Total adjacent_rolls: {}", row_index, col_index, adjacent_rolls);
                 } else{
-                    stringified_row.push('@')
-                }
-                // println!("Position ({},{}) Total available: {}", row_index, col_index, available);
-            } else{
-                stringified_row.push('.');
-                available = check_adjacents(row_index, col_index, &input_matrix);
-                // println!("Position ({},{}) Total available: {}", row_index, col_index, available);
-            }   
+                    stringified_row.push('.');
+                    adjacent_rolls = check_adjacents(row_index, col_index, &input_matrix);
+                    // println!("Position ({},{}) Total adjacent_rolls: {}", row_index, col_index, adjacent_rolls);
+                }   
+            }
+            println!("{}", stringified_row);
+            tmp_vec.push(stringified_row);
         }
-        println!("{}", stringified_row);
+        if lines == tmp_vec {
+            break;
+        } else {
+        lines = tmp_vec;
+        }
     }
-    
-    println!("Total accessible positions: {}", total_accessible);
+
+    println!("Total accessible positions: {}", total_rolls_removed);
     Ok(())
 }
