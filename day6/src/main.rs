2abcee7e6d6b733f023aea649190ab2ee6e34f1c
Kyle Stiers (GNOCB) <kyle.stiers@bayer.com>
2025-12-12 22:50:59 -0600

    working day 6 part 2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -3,19 +3,9 @@ use std::io::{self, BufRead, BufReader};
 use std::path::Path;
 use regex::Regex;
 
-fn transpose<T: Clone>(v: Vec<Vec<T>>) -> Vec<Vec<T>> {
-    if v.is_empty(){
-        return Vec::new();
-    }
-    let transposed = (0..v[0].len())
-        .map(|i| v.iter().map(|inner| inner[i].clone()).collect())
-        .collect();
-    transposed
-}
-
 fn main() -> io::Result<()> {
     // Specify the path to your text file
-    let file_path = Path::new("test.txt");
+    let file_path = Path::new("input.txt");
     
     // Open the file
     let file = File::open(file_path)?;
@@ -39,22 +29,31 @@ fn main() -> io::Result<()> {
         println!("Line {}: {}", row_index, line);
     }
 
+    let mut current_pairs: Vec<usize> = Vec::new();
+    let mut running_tally: Vec<usize> = Vec::new();
+    let mut full_op : usize = 0;
+
+    //loop through the loaded array of strings
     for i in 0..line_array[0].len() {
-        let mut current_pairs: Vec<String> = Vec::new();
-        let mut op :String = ''
+        //placeholder var for op so we can catch the op and use it to condition consolidating all the numbers for that set
+        let mut op :String = "".to_string();
+        //if the col is all spaces reset vars and continue
         if line_array.iter().all(|c| c.chars().nth(i).unwrap() == ' ') {
-            println!("Column {} is all spaces", i);
+            current_pairs = Vec::new();
+            full_op = 0;
         } else {
             let nonspace = line_array.iter().map(|c| c.chars().nth(i).into_iter().collect::<String>()).collect::<Vec<String>>();
-            // top_vals.join("").parse::<i64>().unwrap();
-            println!("Column {} has chars: {:?}", i, &nonspace[..nonspace.len()-1]);
             let num_from_nonspace = nonspace[..nonspace.len()-1].join("").trim().parse::<usize>().unwrap();
-            println!("Parsed number: {}", num_from_nonspace);
-
+            current_pairs.push(num_from_nonspace);
+            op = nonspace[nonspace.len()-1].clone().to_string();
+            if op == "*" {
+                running_tally.push(current_pairs.iter().product());
+            } else if op == "+" {
+                running_tally.push(current_pairs.iter().sum());
+            }
         }
     }
-
-    // println!("Loaded lines: {:?}", line_array);
+    println!("Running tally: {:?}", running_tally.iter().sum::<usize>());
 
     Ok(())
 }
